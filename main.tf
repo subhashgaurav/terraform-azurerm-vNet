128cb64e5e11054e50b51067a841098f1e5b7ee3
subhashgaurav <91779017+subhashgaurav@users.noreply.github.com>
2023-05-15 09:14:57 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+resource "azurerm_resource_group" "rg-1" {
+  name = var.rg_name
+  location = var.location 
+}
+
+resource "azurerm_virtual_network" "vNet-1" {
+  name = var.vNet_name
+  resource_group_name = azurerm_resource_group.name
+  location = azurerm_resource_group.location
+  address_space = ["10.0.0.0/16"]
+  
+  subnet {
+    name = var.subnet_name
+    address_prefix = "10.0.1.0/24"
+    
+  }
+  
+}
